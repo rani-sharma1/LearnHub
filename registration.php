b5009c786bd78615ad8365685f2bc30b89e003f9
rani-sharma1 <ranisharma131012@gmail.com>
2025-09-09 04:57:30 +0530

    Add files via upload
    LearnHub Program

---
+++ registration.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>LearnHub</title>
+</head>
+<div class=" container">
+<div class=" register">
+<form action="regdata.php" method="post">
+<div class="tt-item">
+<h2 class="tt-title">PERSONAL INFORMATION</h2>
+<div class="form-default">
+<div class="form-group">
+<label for="loginInputName">Full NAME *</label>
+<div class="tt-required">* Required Fields</div>
+<input type="text" name="name" class="form-control" placeholder="Enter Full Name"
+required="required">
+</div>
+<div class="form-group">
+<label for="loginInputEmail">E-MAIL *</label> <input type="email" name="email"
+class="form-control" placeholder="Enter E-mail" required="required">
+</div>
+<div class="form-group"> <label for="loginLastName">PHONE NO. *</label> <input
+type="number" name="phone" class="form-control" placeholder="Enter Phone No."
+required="required"> </div>
+<div class="form-group"> <label for="loginInputPassword">PASSWORD *</label> <input
+type="password" name="password" class="form-control" placeholder="Enter Password"
+required="required">
+</div>
+<div class="form-group"> <label for="loginInputPassword">CONFIRM PASSWORD
+*</label> <input type="password" name="confirm_password" class="form-control"
+placeholder="Enter Password" required="required">
+</div>
+<div class="row">
+<div class="col-auto"> <div class="form-group">
+<button class="btn btn-border" type="submit">CREATE</button>
+</div> </div>
+< <div class="form-group">
+<ul class="additional-links">
+<li>or already have a account? <a
+href="http://www.theguiltybites.com/proghub/user/login">Login</a></li> </ul> </div>
+</form> </div>
+</body>
+</html>
